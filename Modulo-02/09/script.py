f553be6de32f85ab2959e710dd441efc175de371
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-10-03 16:51:27 -0300

    Modulo 02-09 completo

--- Modulo-02/09/script.py
+++ Modulo-02/09/script.py
@@ -1,2 +1,14 @@
 import argparse
 
+parser = argparse.ArgumentParser(description="Ejemplo de argparse")
+
+parser.add_argument('-s', '--server', help="Servidor")
+
+# python archivo.py --s hola
+
+args = parser.parse_args()
+
+if args.server:
+    print(args.server)
+else:
+    print("No se recibió el nombre del server")
